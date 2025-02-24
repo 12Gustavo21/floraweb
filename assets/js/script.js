a1466cab2bbe6594087f7efa756479e4171c9d22
12Gustavo21 <gustavo927318@gmail.com>
2025-02-24 14:30:00 -0300

    style: fix responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,20 +19,31 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
-document.querySelectorAll('.group').forEach(group => {
-  const input = group.querySelector('.input');
-  const label = group.querySelector('.label');
+document.querySelectorAll(".group").forEach((group) => {
+  const input = group.querySelector(".input");
+  const label = group.querySelector(".label");
 
-  input.addEventListener('focus', () => {
-      label.classList.add('active');
+  input.addEventListener("focus", () => {
+    label.classList.add("active");
   });
 
-  input.addEventListener('blur', () => {
-      if (input.value.trim() === '') {
-          label.classList.remove('active');
-      }
+  input.addEventListener("blur", () => {
+    if (input.value.trim() === "") {
+      label.classList.remove("active");
+    }
   });
 });
 
 const year = document.querySelector(".year");
 year.innerHTML = new Date().getFullYear();
+
+function voltarPagina() {
+  if (
+    document.referrer.includes(window.location.hostname) &&
+    history.length > 1
+  ) {
+    history.back();
+  } else {
+    window.location.href = "../../index.html"; 
+  }
+}
